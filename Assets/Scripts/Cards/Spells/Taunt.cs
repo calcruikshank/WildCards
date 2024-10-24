2c0688ab6227052c485bff8e01582fa465454113
calcruikshank <calcruikshank@gmail.com>
2024-10-24 01:31:17 -0700

    luf

---
+++ Assets/Scripts/Cards/Spells/Taunt.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class Taunt : TargetedSpell
+{
+    protected override void SpecificSpellAbility()
+    {
+        creatureTargeted.keywords.Add(SpellSiegeData.Keywords.Taunt);
+    }
+}
